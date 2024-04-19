3ada129cad84868d0f14dfb5a22cf09c964476ee
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-04-19 12:08:55 +0300

    fixed files isuue

--- Desktop/MyApp/src/types/index.ts
+++
@@ -1,6 +0,0 @@
-export type VideoSrc={
-    uri:string;
-}
-export type RootStackParamList = {
-    [key: string]: undefined | { id?: number };
-  };
--- Desktop/MyApp/tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./App.{js,jsx,ts,tsx}","./src/screens/**/*.{js,jsx,ts,tsx}" ,"./components/**/*.{js,jsx,ts,tsx}","App.tsx","./src/**/**/*.{js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
